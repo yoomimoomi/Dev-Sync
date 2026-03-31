e6889829d2f8d05338d49f498b7def1d9db845d6
Kevon Franklyn <kevonfranklyn@yahoo.com>
2026-03-31 19:18:53 -0400

    Minor update to the python dependencies to add FastAPI packages through FastAPI[standard] and sample code for the authentication

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI(title = "Devsync")
 @app.get("/")
 async def root():
@@ -7,3 +12,8 @@ async def root():
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+@app.get("/tokentest")
+async def token_test(token : Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
