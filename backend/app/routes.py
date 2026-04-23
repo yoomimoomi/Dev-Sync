cbc706980adfe2b7ab6924ed2bf74b3534962198
kodikrash <nekodiknight117@gmail.com>
2026-04-23 18:05:49 -0400

    schemas and routes

---
+++ backend/app/routes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from .post_schemas import signup, pagepost, comments
+
+db = []
+
+router = APIRouter(
+    prefix="/posts",
+    tags=["Posts"]
+)
+
+@router.post("/signup")
+async def create_user(user: signup):
+    db.append(user)
+    return {"message": "user recorded successfully"}
