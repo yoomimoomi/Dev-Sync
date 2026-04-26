d21ce7186d0481c0efbce2c3f66f1ef8e133a02a
Ash <150209573+yoomimoomi@users.noreply.github.com>
2026-04-26 16:45:19 -0400

    frontend fetch login/registration working

---
+++ backend/build/lib/app/models/account_team.py
@@ -0,0 +1,11 @@
+# from sqlalchemy import CHAR, ForeignKey
+# from sqlalchemy.orm import Mapped, mapped_column
+
+# from app.db.base import Base
+
+
+# class AccountTeam(Base):
+#     __tablename__ = 'accountteams'
+
+#     user_id: Mapped[str] = mapped_column(CHAR(10), ForeignKey('accounts.user_id'), primary_key=True)
+#     team_id: Mapped[str] = mapped_column(CHAR(10), ForeignKey('teams.team_id'), primary_key=True)
